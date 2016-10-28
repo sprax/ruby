d6d7d795906f689337739bb8796f90a39a2ad4a3
sprax <sprax@comcast.net>
2016-10-28 00:37:15 -0400

    diags

--- abs_diff_diags.rb
+++ abs_diff_diags.rb
@@ -1,18 +1,33 @@
 #!/bin/ruby
 
 n = gets.strip.to_i
-a = Array.new(n)
+aa = Array.new(n)
 for a_i in (0..n-1)
     a_t = gets.strip
-    a[a_i] = a_t.split(' ').map(&:to_i)
+    aa[a_i] = a_t.split(' ').map(&:to_i)
 end
 
-sum = 0
-for j in (0...n)
-    sum += a[j][j] - a[j][n-1-j]
+def abs_diff_diags_1(a)
+    sum = 0
+    for j in (0...n)
+        sum += a[j][j] - a[j][n-1-j]
+    end
+    if sum < 0
+        sum = -sum
+    end
+    sum
 end
-if sum < 0
-    sum = -sum
+
+def abs_diff_diags_2(a)
+    sum = 0
+    for j in (0...n/2)
+        sum += a[j][j] - a[j][n-1-j] + a[n-1-j][n-1-j] - a[n-1-j][j]
+    end
+    if sum < 0
+        sum = -sum
+    end
+    sum
 end
-puts sum
+
+puts abs_diff_diags_1(aa) - abs_diff_diags_2(aa)
 
