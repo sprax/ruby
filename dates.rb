849a588a49c47b7fe8c37301d20289efbeabb38e
Sprax Lines <sprax@comcast.net>
2017-03-02 13:50:18 -0500

    tsToDate

--- dates.rb
+++ dates.rb
@@ -12,3 +12,6 @@ dt2 = DateTime.strptime(tss,'%s')
 puts "string(#{tss}) => UTC date #{dt2}"
 
 
+dsz = "2017-03-01T00:00:00Z"
+tsz = DateTime.rfc3339(dsz).to_time.to_i
+puts "rfc3339 date string(#{dsz}) => Unix timestamp #{tsz}"
