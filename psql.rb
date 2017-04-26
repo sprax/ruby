ffc8da128792cd289c47f8fbf5b2f5e5c2f2e030
Sprax Lines <sprax@comcast.net>
2017-04-26 17:59:07 -0400

    more

--- psql.rb
+++ psql.rb
@@ -10,3 +10,25 @@ ORDER BY week;
 SELECT COUNT(*) AS total, SUM(CASE WHEN DATE_PART('dow', created_at) IN (2,6) THEN 1 ELSE 0 END) AS tues_or_sat FROM organizations where id < 150;
 
 
+SELECT TO_CHAR(DATE_TRUNC('week', o.created_at::date), 'YYYY-MM-DD') AS iso_week, COUNT(DISTINCT o.id) AS oc, COUNT(DISTINCT p.id) AS pc, COUNT(DISTINCT t.id) AS tc
+FROM organizations AS o JOIN profiles AS p ON p.organization_id = o.id JOIN bots ON bots.organization_id = o.id JOIN tickets AS t ON t.bot_id = bots.id
+WHERE o.created_at BETWEEN '10 Apr 2017' AND '27 Apr 2017' GROUP BY iso_week ORDER BY iso_week;
+
+SELECT TO_CHAR(DATE_TRUNC('week', o.created_at::date), 'YYYY-MM-DD') AS iso_week, COUNT(DISTINCT o.id) AS oc, COUNT(DISTINCT p.id) AS pc, COUNT(DISTINCT t.id) AS tc, SUM(DISTINCT CASE WHEN p.id = t.requestor_profile_id THEN 1 ELSE 0 END) AS apc FROM organizations AS o JOIN profiles AS p ON p.organization_id = o.id JOIN bots ON bots.organization_id = o.id JOIN tickets AS t ON t.bot_id = bots.id where o.created_at BETWEEN '10 Apr 2017' AND '27 Apr 2017' GROUP BY iso_week ORDER BY iso_week;
+  iso_week | oc | pc  | tc | apc 
+-----------+----+-----+----+-----
+2017-04-10 | 11 | 224 | 95 |   1
+2017-04-17 |  7 |  65 | 52 |   1
+2017-04-24 |  8 | 210 | 54 |   1
+(3 rows)
+
+select TO_CHAR(DATE_TRUNC('week', o.created_at::date), 'YYYY-MM-DD') AS iso_week, COUNT(DISTINCT o.id) AS oc, COUNT(DISTINCT p.id) AS pc, COUNT(DISTINCT t.id) AS tc, COUNT((p.id IN (SELECT profile_id FROM workflow_states))) AS apc FROM organizations AS o JOIN profiles AS p ON p.organization_id = o.id JOIN bots ON bots.organization_id = o.id JOIN tickets AS t ON t.bot_id = bots.id where o.created_at BETWEEN '10 Apr 2017' AND '27 Apr 2017' GROUP BY iso_week ORDER BY iso_week;
+  iso_week  | oc | pc  | tc | apc  
+  ------------+----+-----+----+------
+       2017-04-10 | 11 | 224 | 95 | 1953
+   2017-04-17 |  7 |  65 | 52 |  450
+    2017-04-24 | 10 | 217 | 67 | 1751
+    (3 rows)
+
+
+
