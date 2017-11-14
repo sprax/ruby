e47642dc624a040aed95ebeebf46c37e1989a73c
Sprax Lines <sprax@comcast.net>
2017-11-14 12:32:34 -0500

    vars

--- es.sh
+++ es.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/sh
+# See: https://stackoverflow.com/questions/17029902/using-curl-post-with-variables-defined-in-bash-script-functions
 
-term=${1:-knowledge}
+query_type=${1:-query_string}
+query_term=${2:-knowledge}
+def_fields=${3:-description}
 
 gen_data()
 {
 cat <<EOF
 {
     "query": {
-        "query_string": {
-            "query": "$term",
-            "default_field": "description"
+        "$query_type": {
+            "query": "$query_term",
+            "default_field": "$def_fields"
         }
     }
 }
@@ -20,4 +23,7 @@ EOF
 curl "http://localhost:9200/get-together/_search" -d "$(gen_data)"
 
 echo
-echo "term: $term"
+echo "query_type: $query_type"
+echo "query_term: $query_term"
+echo "def_fields: $def_fields"
+
