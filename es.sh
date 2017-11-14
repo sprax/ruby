70330d163b1a892ad0f8beea4cc5919d453e1f34
Sprax Lines <sprax@comcast.net>
2017-11-14 12:20:56 -0500

    gen_data for curl

--- es.sh
+++ es.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/sh
 
-curl "http://localhost:9200/get-together/_search" -d'
+term=${1:-knowledge}
+
+gen_data()
+{
+cat <<EOF
 {
     "query": {
         "query_string": {
-            "query": "elasticsearch hadoop knowledge", "default_field": "description"
+            "query": "$term",
+            "default_field": "description"
         }
     }
-}' 
+}
+EOF
+} 
 
+
+curl "http://localhost:9200/get-together/_search" -d "$(gen_data)"
+
+echo
+echo "term: $term"
