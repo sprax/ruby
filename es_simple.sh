d4fb817a97598361fb953510705f5f59496094c1
Sprax Lines <sprax@comcast.net>
2017-11-16 15:10:29 -0500

    simple

--- es_simple.sh
+++ es_simple.sh
@@ -1,24 +1,23 @@
 #!/usr/local/bin/bash
 # See: https://stackoverflow.com/questions/17029902/using-curl-post-with-variables-defined-in-bash-script-functions
 
-query_type="simple_query_string"
-query_term=${1:-"Group of Clojure"}
+query_type=simple_query_string
+query_term=${1:-"from Denver"}
 def_field1=${2:-description}
 def_field2=${3:-name}
-multi_flag=${4:-"OR|AND|PREFIX"}
-
+def_operat=${4:-"OR|AND|PREFIX"}
 
 gen_data()
 {
 cat <<EOF
 {
-    "query": {
-        "simple_query_string": {
-            "query": "$query_term",
-            "fields": ["$def_field1", "$def_field2"],
-            "flags" : "$multi_flag"
-        }
-    },
+  "query": {
+    "$query_type" : {
+        "query": "+($query_term | search) -frittata",
+        "fields": ["$def_field1", "$def_field2"],
+        "default_operator": "and"
+    }
+  }
 }
 EOF
 } 
