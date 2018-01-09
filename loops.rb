4afe7e203553a8ab447c9ed5686131f0843cdf3d
Sprax Lines <sprax@comcast.net>
2018-01-09 14:12:21 -0500

    times

--- loops.rb
+++ loops.rb
@@ -1,37 +1,41 @@
 # Ruby script using loops
-# that would be trapped by a histogram. 
+# that would be trapped by a histogram.
 
 def main_loops()
 
     histogram = [-1, 2, 32, -4, 4, 44, 2, 38, 0]
     length = histogram.length;
-    
+
     for j in (0...length)	# same syntax works in Python
-        puts( histogram[j])
+      puts( histogram[j])
     end
 
     puts "\nNow backwards downto:";
     length.downto(0) do |j|
-        puts( histogram[j])
+      puts( histogram[j])
     end
 
     puts "\nBackwards range.to_a.reverse:";
     for j in (0...length).to_a.reverse do
-        puts( histogram[j])
+      puts( histogram[j])
     end
 
     puts "\nBackwards until:";
     j = length;
     begin
-	j = j - 1;
-        puts( histogram[j])
+	    j = j - 1;
+      puts( histogram[j])
     end until j == 0
 
     puts "\nBackwards while:";
     j = length - 1;
     while (j >= 0) do
-        puts( histogram[j])
-	j -= 1;
+      puts( histogram[j])
+	    j -= 1;
+    end
+
+    4.times do
+      puts "a"
     end
 
 end
@@ -41,4 +45,3 @@ def main()
 end
 
 main
-
